f2630c7b2c493f4c9872544b520aef4524a104b7
ClaireK <c_l_king@hotmail.com>
2019-10-17 14:08:39 +0200

    Updates

---
+++ precache-manifest.3357f28a14c692a7d35e83ae556eecd5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd1e86d6432bea7d5be984562d1848a0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "034ba1d623b44c603e08",
+    "url": "/monsters-rolodex/static/css/main.c3651683.chunk.css"
+  },
+  {
+    "revision": "e89ff0de3480fcf68967",
+    "url": "/monsters-rolodex/static/js/2.9573cf0d.chunk.js"
+  },
+  {
+    "revision": "034ba1d623b44c603e08",
+    "url": "/monsters-rolodex/static/js/main.778064a8.chunk.js"
+  },
+  {
+    "revision": "e9f306bcf1c509cf0d18",
+    "url": "/monsters-rolodex/static/js/runtime-main.3e4d36ee.js"
+  }
+]);
